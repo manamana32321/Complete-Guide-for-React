c6d2a2fc25878f0c27b750dd01a19caca6224472
manamana32321 <manamana32321@gmail.com>
2023-02-25 15:23:28 +0900

    corrected first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
-import EventDetailPage, {
-  loader as eventDetailLoader,
-  action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage, { action as newEventAction } from './pages/NewEvent';
-import RootLayout from './pages/Root';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomePage /> },
-      {
-        path: 'events',
-        element: <EventsRootLayout />,
-        children: [
-          {
-            index: true,
-            element: <EventsPage />,
-            loader: eventsLoader,
-          },
-          {
-            path: ':eventId',
-            id: 'event-detail',
-            loader: eventDetailLoader,
-            children: [
-              {
-                index: true,
-                element: <EventDetailPage />,
-                action: deleteEventAction,
-              },
-              { path: 'edit', element: <EditEventPage /> },
-            ],
-          },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
-        ],
-      },
-    ],
-  },
-]);
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
 }
 
 export default App;
